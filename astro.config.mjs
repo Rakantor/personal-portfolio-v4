fcbc8027da1e77e12734426c19b4e0e77a4eef65
Rakantor <rakantor.dev@gmail.com>
2026-06-30 14:11:17 +0200

    build: specify deploy domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://mave.dev',
 	i18n: {
 		locales: ['en', 'de'],
 		defaultLocale: 'en',
